855ec2cc034521017af0689f1432a03c24ababda
emrrar <avatareigan@gmail.com>
2021-10-04 17:31:34 +0300

    Wrote the current full functionality of the movie service.

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -10,7 +10,22 @@ async function get(id) {
     return await jsonRequest(`${baseUri}/${id}`);
 }
 
+async function create(movie) {
+    return await jsonRequest(`${baseUri}`, 'Post', movie);
+}
+
+async function edit(movie, id) {
+    return await jsonRequest(`${baseUri}/${id}`, 'Put', movie);
+}
+
+async function del(id) {
+    return await jsonRequest(`${baseUri}/${id}`, 'Delete');
+}
+
 export default {
     getAll,
-    get
+    get,
+    create,
+    edit,
+    del
 }
